98796441ce2e89a3474d2b2673990254d7131567
Regent Lee <regentlee.com@gmail.com>
2019-06-27 20:34:22 +0900

    Initial layers.py

---
+++ layers.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import numpy as np
+
+
+class SelectiveDesensitization:
+    def __init__(self):
+        pass
+
+
+class FullyConnected:
+    def __init__(self):
+        pass
+
+
+class SGN:
+    def __init__(self):
+        pass

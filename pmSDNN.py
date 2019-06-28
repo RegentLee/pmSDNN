82f8d3ecf678ab5734ea49741983596c92000e9f
Regent Lee <regentlee.com@gmail.com>
2019-06-28 21:53:25 +0900

    Initial pmSDNN.py

--- pmSDNN.py
+++ pmSDNN.py
@@ -1,1 +1,14 @@
 # coding: utf-8
+from patterncoding import PatternCoding
+from layers import SelectiveDesensitization, FullyConnected, SGN, PotentialLoss
+
+
+class pmSDNN:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
